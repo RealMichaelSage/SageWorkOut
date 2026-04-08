a6e1cd40fa82bfa01f8d03042e290e99a1b3dd3d
Michael Sage <michaelsage@MacBook-SAGE.local>
2026-04-08 12:01:24 +0300

    Fix Service Worker update logic to force activation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sageworkout-v9';
+const CACHE_NAME = 'sageworkout-v10';
 const ASSETS = [
   './',
   './index.html',
@@ -10,8 +10,20 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME)
+            .map((key) => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
